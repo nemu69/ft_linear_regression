a41dcf78f58100f796198b98cffbeb07aa31e71a
nemu69 <gametwoofficiel@gmail.com>
2021-11-03 02:49:44 +0100

    finish i guess

--- main.py
+++ main.py
@@ -1,61 +1,144 @@
-# Import pandas as pd
-import pandas as pd
 import sys
+import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
  
-def estimate_coef(x, y):
-    # number of observations/points
-    n = np.size(x)
- 
-    # mean of x and y vector
-    m_x = np.mean(x)
-    m_y = np.mean(y)
- 
-    # calculating cross-deviation and deviation about x
-    SS_xy = np.sum(y*x) - n*m_y*m_x
-    SS_xx = np.sum(x*x) - n*m_x*m_x
- 
-    # calculating regression coefficients
-    b_1 = SS_xy / SS_xx
-    b_0 = m_y - b_1*m_x
- 
-    return (b_0, b_1)
- 
-def plot_regression_line(x, y, b):
-	# plotting the actual points as scatter plot
-	plt.scatter(x, y, color = "g", marker = "o", s = 50)
+
+class Linear_Regression:
+	def __init__(self, X, Y):
+		self.X = X
+		self.Y = Y
+		self.b = [0, 0]
+	  
+	def update_coeffs(self, learning_rate):
+		Y_pred = self.predict()
+		Y = self.Y
+		m = len(Y)
+		self.b[0] = self.b[0] - (learning_rate * ((1/m) * 
+								  np.sum(Y_pred - Y)))
+  
+		self.b[1] = self.b[1] - (learning_rate * ((1/m) * 
+								  np.sum((Y_pred - Y) * self.X)))
+		return self.b
+  
+	def predict(self, X=[]):
+		Y_pred = np.array([])
+		if not X: X = self.X
+		b = self.b
+		for x in X:
+			Y_pred = np.append(Y_pred, b[0] + (b[1] * x))
+  
+		return Y_pred
+	  
+	def get_current_accuracy(self, Y_pred):
+		p, e = Y_pred, self.Y
+		n = len(Y_pred)
+		return 1-sum(
+			[
+				abs(p[i]-e[i])/e[i]
+				for i in range(n)
+				if e[i] != 0]
+		)
+  
+	def compute_cost(self, Y_pred):
+		m = len(self.Y)
+		J = (1 / 2*m) * (np.sum(Y_pred - self.Y)**2)
+		return J
+  
+	def plot_best_fit(self, Y_pred, fig):
+				plt.figure(fig)
+				plt.scatter(self.X, self.Y, color='b')
+				plt.plot(self.X, Y_pred, color='g')
+				plt.show()
+
+	def precision(self):
+		Y = self.Y
+		y_pred = self.predict()
+		u = ((Y - y_pred)**2).sum()
+		v = ((Y - Y.mean())**2).sum()
+		return 1 - u/v
+  
+
+def normalize(x):
+	return (x - min(x)) / (max(x) - min(x))
+
 	
-	# estimatePrice(mileage) = θ0 + (θ1 ∗ mileage)
-	y_pred = b[0] + b[1]*x
-	print(y_pred)
-	
-	# plotting the regression line
-	plt.plot(x, y_pred, color = "r")
-	
-	# putting labels
-	plt.xlabel('mileage')
-	plt.ylabel('price')
-	
-	# function to show plot
-	plt.show()
+
 
 def main() :
 	args = sys.argv[1:]
-	# args is a list of the command line args
-	print(args[0])
-	# Import the cars.csv data: cars
-	data = pd.read_csv(args[0], index_col=False).sort_values(by=['km'], ascending=False)
 
-	arr1 = data['km']
-	arr2 = data['price']
-	print(arr1)
-	print(arr2)
-	b = estimate_coef(arr1, arr2)
-	print(b)
-	plot_regression_line(arr1, arr2, b)
+	# DataSet
+	data = pd.read_csv(args[0], index_col=False)
+
+	X = data['km']
+	Y = data['price']
+
+	# check if data is empty 
+	if len(X) == 0 or len(Y) == 0:
+		print("No data to process")
+		return
+	
+	# chck is dta has a string
+	if isinstance(X[0], str) or isinstance(Y[0], str):
+		print("Data must be numeric")
+		return
+
+	# check if an data is not a number or is negative
+	if min(X) < 0 or min(Y) < 0:
+		print("Invalid data")
+		return
+
+	#if not isinstance(X[0], (int, float)) or not isinstance(Y[0], (int, float))
+	# Stabilize value
+	X = normalize(X)
+
+	# Building the model
+	print("X :", X)
+	print("Y :", Y)
+	regressor = Linear_Regression(X, Y)
+  
+	iterations = 0
+	steps = 1000
+	learning_rate = 0.1
+	costs = []
+	  
+	#original best-fit line
+	Y_pred = regressor.predict()
+	regressor.plot_best_fit(Y_pred, 'Initial Best Fit Line')
+	print("Loading...")
+	while 1 :
+		Y_pred = regressor.predict()
+		cost = regressor.compute_cost(Y_pred)
+		costs.append(cost)
+		b = regressor.update_coeffs(learning_rate)
+
+		iterations += 1
+		if steps - iterations == 0:
+			print("Finish !")
+			print(iterations, "epochs elapsed and precision of ", regressor.precision())
+			break
 	
 
+	regressor.plot_best_fit(Y_pred, 'Final Best Fit Line')
+
+	with open("predict.py", "r") as in_file:
+		buf = in_file.readlines()
+
+	with open("predict.py", "w") as out_file:
+		for line in buf:
+			if line.find("theta0 =") != -1:
+				line = "theta0 = " + str(b[0]) + "\n"
+			if line.find("theta1 =") != -1:
+				line = "theta1 = " + str(b[1]) + "\n"
+			if line.find("data =") != -1:
+				line = "data = \"" + str(args[0]) + "\"\n"
+			out_file.write(line)
+	
+	# Plot to verify cost fuction decreases
+	plt.figure('Cost')
+	plt.plot(range(iterations), costs, color='b')
+	plt.show()
+
 if __name__ == "__main__":
-    main()
+	main()
--- predict.py
+++ predict.py
@@ -1,15 +1,46 @@
 # Import pandas as pd
 import pandas as pd
 
+theta0 = 7990.866013397061
+theta1 = -4610.2051096833275
+data = "data.csv"
 
+def normalize(x, init):
+    return (x - min(init)) / (max(init) - min(init))
 
 def main() :
+	set = pd.read_csv(data, index_col=False)
+
+	X = set['km']
+	Y = set['price']
+
+	# check if data is empty 
+	if len(X) == 0 or len(Y) == 0:
+		print("No data to process")
+		return
+	
+	# chck is dta has a string
+	if isinstance(X[0], str) or isinstance(Y[0], str):
+		print("Data must be numeric")
+		return
+
+	# check if an data is not a number or is negative
+	if min(X) < 0 or min(Y) < 0:
+		print("Invalid data")
+		return
+
 	while True :
 		try:
-			prompt = int(input("Enter the mileage: "))
-			print(prompt)
+			prompt = int(input("Enter the mileage or negative to exit : "))
+			if prompt < 0 :
+				print("Exit")
+				break
+				
+			else :
+				# estimateP rice(mileage) = θ0 + (θ1 ∗ mileage)
+				print(int(theta0 + (theta1 * normalize(prompt, X))))
 			
-			break
+
 		except ValueError:
 			print("This is not a valid number. Try Again !")
 
